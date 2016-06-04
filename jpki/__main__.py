ca18518750905c8c0d999cc65199cc3faca55477
HAMANO Tsukasa <code@cuspy.org>
2016-06-04 14:42:36 +0900

    update

--- jpki/__main__.py
+++ jpki/__main__.py
@@ -1,7 +1,9 @@
+import sys
 from . import app
 
 def main():
     app.cli()
 
 if __name__ == '__main__':
+    sys.argv[0] = 'jpki'
     main()
--- jpki/app.py
+++ jpki/app.py
@@ -5,16 +5,18 @@ from . import __version__
 import click
 
 @click.group()
-@click.version_option(prog_name='jpki', version=__version__)
+@click.version_option(version=__version__)
 @click.option('-v', '--verbose', count=True)
 @click.pass_context
 def cli(ctx, verbose):
     ctx.obj = {}
 
 @cli.command(help='show cert')
+@click.pass_context
 def cert(ctx):
     pass
 
 @cli.command(help='sign')
+@click.pass_context
 def sign(ctx):
     pass
