423a345c676aa741e7101ecb410c2456f053944a
HAMANO Tsukasa <code@cuspy.org>
2016-06-17 00:17:26 +0900

    add find_ap command

--- tool.go
+++ tool.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	//"os"
-	//"fmt"
+	"fmt"
 	"github.com/urfave/cli"
 )
 
@@ -12,13 +12,58 @@ var toolCommands = []cli.Command {
 		Usage: "Beep off for ACS Reader",
 		Action: beepOff,
 	},
+	{
+		Name: "find_ap",
+		Usage: "search AP",
+		Action: findAP,
+		Flags: commonFlags,
+	},
 }
 
 func beepOff(c *cli.Context) error {
-	reader := NewReader()
+	reader := NewReader(c)
 	defer reader.Finalize()
-	card := reader.WaitForCard()
-	tx(card, "FF 00 52 00 00")
+	reader.WaitForCard()
+	reader.Tx("FF 00 52 00 00")
 	return nil
 }
 
+func findAP(c *cli.Context) error {
+	//var prefix = []byte{0xD3, 0x92}
+	var prefix = []byte{}
+
+	reader := NewReader(c)
+	defer reader.Finalize()
+	reader.WaitForCard()
+	ret := findDF(reader, prefix)
+	for _, ap := range ret {
+		fmt.Printf("found ap: % X\n", ap)
+	}
+	return nil
+}
+
+func findDF(reader *Reader, prefix []byte) [][]byte {
+	var tmp [][]byte
+	i := len(prefix)
+	l := i + 1
+	buf := append(prefix, 0)
+	for n := 0; n<255; n++ {
+		buf[i] = byte(n)
+		apdu := "00 A4 04 0C " +
+			fmt.Sprintf("%02X ", l) +
+			fmt.Sprintf("% X", buf)
+		sw1, sw2, _ := reader.Tx(apdu)
+		if(sw1 == 0x90 && sw2 == 0x00){
+			ret := findDF(reader, buf)
+			if len(ret) == 0 {
+				//fmt.Printf("found ap % X\n", buf)
+				dup := make([]byte, len(buf))
+				copy(dup, buf)
+				tmp = append(tmp, dup)
+			}else{
+				tmp = append(tmp, ret...)
+			}
+		}
+	}
+	return tmp
+}
