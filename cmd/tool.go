14e2b3817a21ba755b0745422ac0329dcf609a01
HAMANO Tsukasa <hamano@osstech.co.jp>
2018-04-29 16:22:34 +0900

    refactoring

--- cmd/tool.go
+++ cmd/tool.go
@@ -87,18 +87,14 @@ func findAP(cmd *cobra.Command, args []string) error {
 func findDF(reader *libmyna.Reader, prefix []byte) [][]byte {
 	var tmp [][]byte
 	i := len(prefix)
-	l := i + 1
 	buf := append(prefix, 0)
 	for n := 0; n < 255; n++ {
 		buf[i] = byte(n)
-		apdu := "00 A4 04 0C " +
-			fmt.Sprintf("%02X ", l) +
-			fmt.Sprintf("% X", buf)
-		sw1, sw2, _ := reader.Tx(apdu)
-		if sw1 == 0x90 && sw2 == 0x00 {
+		err := reader.SelectDF(libmyna.ToHexString(buf))
+		if err == nil {
+			fmt.Printf("FOUND: % X\n", buf)
 			ret := findDF(reader, buf)
 			if len(ret) == 0 {
-				//fmt.Printf("found ap % X\n", buf)
 				dup := make([]byte, len(buf))
 				copy(dup, buf)
 				tmp = append(tmp, dup)
